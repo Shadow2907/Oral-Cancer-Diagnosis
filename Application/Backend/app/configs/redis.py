3274f5aa3850c44d6acfcc57657026dcd00d2724
Shadow2907 <hieu29072004@gmail.com>
2025-04-11 15:07:48 +0700

    backend

--- Application/Backend/app/configs/redis.py
+++ Application/Backend/app/configs/redis.py
@@ -11,10 +11,9 @@ import redis
 load_dotenv()
 
 redis_client = Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'),
-    port=int(os.getenv('REDIS_PORT', 6379)),
-    db=int(os.getenv('REDIS_DB', 0)),
-    password=os.getenv('REDIS_PASSWORD'),
+    host=os.getenv('REDISHOST', 'localhost'),
+    port=int(os.getenv('REDISPORT', 6379)),
+    password=os.getenv('REDISPASSWORD'),
     decode_responses=True
 )
 
