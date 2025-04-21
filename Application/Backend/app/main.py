c0fd76471998aac4a451c394d9dfe062e7e6523f
Nguyễn Trung Hiếu <152794421+Shadow2907@users.noreply.github.com>
2025-04-21 19:02:39 +0700

    Backend

--- Application/Backend/app/main.py
+++ Application/Backend/app/main.py
@@ -11,29 +11,6 @@ app = FastAPI()
 # Add GZip compression
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-# origins = ["http://localhost:5173", "http://52.184.86.56:5173"]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all methods
-#     allow_headers=["*"],  # Allows all headers
-# )
-
-# origins = [
-#    "http://192.168.211.:8000",
-#    "http://localhost",
-#    "http://localhost:8080",
-# ]
-# app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=origins,
-#    allow_credentials=True,
-#    allow_methods=["*"],
-#    allow_headers=["*"],
-# )
-
-
 @app.on_event("startup")
 async def on_startup():
     await init_db()
