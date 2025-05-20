6413889177baa2e2e3347d6900eed207605fca57
HoangDuongd <157714649+HoangDuongd@users.noreply.github.com>
2025-05-20 20:51:11 +0700

    Update create_polygons_table.sql

--- DataBase/create_polygons_table.sql
+++ DataBase/create_polygons_table.sql
@@ -1,7 +1,7 @@
 CREATE EXTERNAL TABLE metadata_jsonfile.annotations (
     image_id STRING,
     annotation_id INT,
-    polygon_type STRING,
+    annotation_type STRING,
     annotation_json STRING
 )
 PARTITIONED BY (partition_0 STRING)
@@ -14,8 +14,8 @@ TBLPROPERTIES ('has_encrypted_data'='false');
 INSERT INTO metadata_jsonfile.polygons
 SELECT 
     m.id AS image_id,
-    CAST(json_extract_scalar(json_parse(annotation), '$.properties.polygon_id') AS INTEGER) AS polygon_id,
-    json_extract_scalar(json_parse(annotation), '$.geometry.type') AS polygon_type,
+    CAST(json_extract_scalar(json_parse(annotation), '$.properties.polygon_id') AS INTEGER) AS annotation_id,
+    json_extract_scalar(json_parse(annotation), '$.geometry.type') AS annotation_type,
     annotation AS annotation_json,
     m.partition_0
 FROM metadata_jsonfile.metadata m
